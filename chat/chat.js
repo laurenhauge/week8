12d93b6e8d58e2e1e1acbcbf8f6f02611d66aea5
Lauren Hauge <lauren.hauge@kellogg.northwestern.edu>
2021-05-20 21:02:07 -0500

    v1

--- chat/chat.js
+++ chat/chat.js
@@ -35,14 +35,21 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     // 🔥 Step 4 begins here
     // get a reference to the "submit message" button
+    let submitMessage = document.querySelector(`#submit-message`)
 
     // handle the "submit message" button being clicked
+    submitMessage.addEventListener(`click`, async function(event) { 
 
       // ignore the default form submit behavior
+      event.preventDefault()
 
       // get a reference to the "message" input box
+      let messageBox = document.querySelector(`#message`)
 
       // get the message that was typed in the "message" input box
+      let message = messageBox.value
+      console.log(message)
+
 
       // the lambda function to create a message is already written and available at:
       // https://kiei451.com/.netlify/functions/create_message?userName=_______&body=________
@@ -52,27 +59,37 @@ firebase.auth().onAuthStateChanged(async function(user) {
       // if successfully called, the return value of the lambda is an Array of all messages
 
       // build the URL of the lambda function
+      let url = `https://kiei451.com/.netlify/functions/create_message?userName=${user.displayName}&body=${message}`
 
       // Fetch the url, wait for a response, store the response in memory
+      let response = await fetch(url) 
 
       // Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
+      console.log(json)
 
       // Grab a reference to the element with class name "messages" in memory
+      let messages = document.querySelector(`.messages`)
 
       // Clear anything already in the "messages" element
+      messages.innerHTML = ""
 
       // Loop through the JSON data, for each Object representing a message:
-
+      for (let i=0; i<json.length; i++){
         // Store each object ("message") in memory
+        let messageObject = json[i]
 
         // Create some markup using the post data, insert into the "messages" element
-        // Sample HTML to use:
-        // <div class="md:mx-0 mx-4 mt-8 w-1/2 mx-auto">
-        //   <span class="font-bold">Brian says... </span>
-        //   I love tacos!
-        // </div>
+        messages.insertAdjacentHTML(`beforeend`, `<div class="md:mx-0 mx-4 mt-8 w-1/2 mx-auto">
+        <span class="font-bold">${messageObject.userName} says ${messageObject.body}
+        </div>`)
+        // console.log(messageObject)
+      }
 
-      // clear out the messages input box
+      messageBox.value = ``  
+    })
+    
+      
   } else {
     // user is not logged-in, so show login
     // Initializes FirebaseUI Auth
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,12 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
+  apiKey: "AIzaSyBrOHwskRvkXhf2Qeu_wpyo2c3O8KejeeY",
+  authDomain: "kiei-451-7ad34.firebaseapp.com",
+  projectId: "kiei-451-7ad34",
+  storageBucket: "kiei-451-7ad34.appspot.com",
+  messagingSenderId: "92694867738",
+   appId: "1:92694867738:web:b60db147be1becd9aa06a3"
 } // replace
 
 if (!firebase.apps.length) {
